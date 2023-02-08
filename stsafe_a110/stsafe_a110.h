7e05a75df4e290e3b36c0fe3795cd60dc42274c0
Clément Laigle <c.laigle@catie.fr>
2023-02-08 16:06:38 +0100

    Add echo, read and update data partition functions

--- stsafe_a110/stsafe_a110.h
+++ stsafe_a110/stsafe_a110.h
@@ -5,12 +5,26 @@
 #ifndef CATIE_SIXTRON_STSAFEA110_H_
 #define CATIE_SIXTRON_STSAFEA110_H_
 
+#include "mbed.h"
+#include "stsafea_core.h"
+
 namespace sixtron {
 
 class STSafeA110 {
 
 public:
     STSafeA110();
+
+    StSafeA_ResponseCode_t init();
+
+    StSafeA_ResponseCode_t echo(uint8_t *buffer_in, uint8_t *buffer_out, size_t length);
+
+    StSafeA_ResponseCode_t update_data_partition(uint8_t zone_index, uint8_t *buf, uint16_t length);
+
+    StSafeA_ResponseCode_t read_data_partition(uint8_t zone_index, uint8_t *buf, uint16_t length);
+
+private:
+    StSafeA_Handle_t _stsafe_handler;
 };
 
 } // namespace sixtron
